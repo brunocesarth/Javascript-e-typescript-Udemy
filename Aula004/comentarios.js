162bfd7f14f7f304031f0bdbc8cf957e11fdde3a
Bruno Cesar Machado Thobias <bruno.cm.thobias@hotmail.com>
2022-06-09 19:09:41 -0300

    Aula004 - Comentários

---
+++ Aula004/comentarios.js
@@ -0,0 +1,11 @@
+//Comentário de LINHA
+console.log('Olá, mundo!'); //Aqui tem outro comentário
+console.log('Linha 1');
+
+/* Comentário de BLOCO
+ * console.log('Linha 2');
+ * console.log('Linha 3');
+ * console.log('Linha 4'); 
+ */
+
+//Vai ser ignorado
