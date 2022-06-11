618c57858212b374b028bd1bd05204ccf529b29b
Bruno Cesar Machado Thobias <bruno.cm.thobias@hotmail.com>
2022-06-10 22:29:41 -0300

    Aula 7 - JS & TS

---
+++ Aula007/index.js
@@ -0,0 +1,17 @@
+// + - * /
+// String = Text | Number = Número
+const primeiroNumero = 5;
+const segundoNumero = 10;
+const resultado = primeiroNumero * segundoNumero;
+const resultadoDuplicado = resultado * 2;
+let resultadoTriplicado = resultado * 3;
+
+console.log(resultado);
+console.log(resultadoDuplicado);
+console.log(resultadoTriplicado);
+
+resultadoTriplicado = resultadoTriplicado + 5;
+console.log(resultadoTriplicado);
+console.log(primeiroNumero);
+
+console.log(typeof primeiroNumero); //Mostra o tipo da variável
