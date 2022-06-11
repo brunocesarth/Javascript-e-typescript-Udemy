537f3dcd4bcd7b1d49e2091b8c42b2ca1052d069
Bruno Cesar Machado Thobias <bruno.cm.thobias@hotmail.com>
2022-06-11 20:25:55 -0300

    Aula 11 - JS & TS
    Resolução do exercício

---
+++ Aula011/js/script.js
@@ -0,0 +1,17 @@
+//MINHA RESOLUÇÃO DO EXERCÍCIO PROPOSTO
+const num1 = Number(prompt('Digite um número?'));
+const num2 = Number(prompt('Digite outro número?'));
+
+let resultado = num1 + num2;
+
+alert(`O resultado é ${resultado}.`); //TEMPLETE STRINGS
+
+
+/*  let num1 = prompt('Digite um número:');
+**  let num2 = prompt('Digite outro número:');
+*
+*   num1 = Number(num1);
+*   num2 = Number(num2);
+*
+*   alert(`O resultado da sua conta foi: ${num1 +num2}`); 
+*/
