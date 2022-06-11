fde888b17cebdb9f28b3dd3779439c28937c6779
Bruno Cesar Machado Thobias <bruno.cm.thobias@hotmail.com>
2022-06-11 13:47:35 -0300

    Aula 9 - JS & TS (Parte 2)

---
+++ Aula009/index2.js
@@ -0,0 +1,24 @@
+//String, number, undefined, null, boolean
+//STRING
+const nome = 'Bruno';
+const nome1 = "Bruno";
+const nome2 = `Bruno`;//Para usar TEMPLATE STRINGS usar cráse
+
+//NUMBER
+const num1 = 10;
+const num2 = 10.52;
+
+//UNDEFINED = Não aponta para local nenhum na memória
+//Configurado pelo sistema
+let nomeAluno;
+
+//NULO = Não aponta para local nenhum na memória
+//Utilizado para desconfigurar uma variável
+//Configurado pelo programador
+const sobrenomeAluno = null;
+
+//Boolean = Verdadeiro ou Falso(true or false) = Lógico
+const aprovado = true;
+
+//Trás o tipo da variável (Primeiro vem o tipo depois vem o valor)
+console.log(typeof nome, nome);
