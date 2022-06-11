24ebbc8f8607175d819e80a0efb82f01ef2b320a
Bruno Cesar Machado Thobias <bruno.cm.thobias@hotmail.com>
2022-06-10 21:12:45 -0300

    Aula 6 - JS & TS

---
+++ Aula006/index.js
@@ -0,0 +1,13 @@
+let nome = 'João';
+
+console.log(nome,'nasceu em 1984.');
+console.log('Em 2000', nome,'conheceu Maria.');
+console.log(nome,'casou-se com Maria em 2012.');
+console.log('Maria teve 1 filho com', nome,'em 2015.');
+console.log('O filho de',nome,'se chama Eduardo.');
+
+let nome1; //Declarou a variável
+nome1 = 'Qualquer valor'; //Inicializando a variável
+console.log(nome1);
+nome1 = 'Otávio'; //Alterando o valor da variável
+console.log(nome1);
