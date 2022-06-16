1ede29e105f4a80119f5ae2a09998f00562daf08
Bruno Cesar Machado Thobias <bruno.cm.thobias@hotmail.com>
2022-06-16 12:25:38 -0300

    Continuação da aula 16 - JS & TS

--- Aula016/index.js
+++ Aula016/index.js
@@ -25,3 +25,20 @@ console.log(alunos);
 
 alunos.pop(); //Remove do final do array
 console.log(alunos);
+const removidoDoFinal = alunos.pop(); // Salva o elemento removido do array em uma variável
+console.log(removidoDoFinal);//exibindo o elemento removido
+console.log(alunos);
+const removidoDoComeco = alunos.shift(); //Remove do começo do array
+console.log(removidoDoComeco);
+
+delete alunos[1];//Deleto o elemento, mas não altero o índice, então o índice fica vazio(undefined)
+
+console.log(alunos[50]);//Consigo acessar um índice que não existe
+
+console.log(alunos)
+console.log(alunos.slice(0, 3)); //Escolho qual parte do array eu quero pegar, aqui vai do 0 ao 2, mas preciso usar o 3
+
+console.log(alunos.slice(0, -2)); //Ele para a exibição deixando a quantidade de números selecionados
+
+console.log(typeof alunos);//Array é um object
+console.log(alunos instanceof Array); //Perguntando se alunos é uma instância de array, retorna true ou false
