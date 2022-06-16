2641cc9469369f3e3fc6ba6951161054c40ff788
Bruno Cesar Machado Thobias <bruno.cm.thobias@hotmail.com>
2022-06-16 16:46:46 -0300

    Update index.js

--- Aula018/index.js
+++ Aula018/index.js
@@ -30,11 +30,12 @@ console.log(pessoa2.idade);
 
 //Criando função para criação de pessoa | Essa função é chamada de factory, ela cria objetos
 function criaPessoa(nome, sobrenome, idade){
-    return {
-        nome: nome,
-        sobrenome: sobrenome,
-        idade: idade
-    };
+    return { nome, sobrenome, idade };
+    /*  Como tenho valores com o mesmo nome não preciso colocar assim, posso colocar só o nome, como mostrado acima
+**  nome: nome,
+*   sobrenome: sobrenome,
+*   idade: idade 
+*/
 }
 
 const pessoa001 = criaPessoa('Jackson','Teller',30);//As informações são chamadas de argumentos, e são passadas para os parâmetros da função
@@ -45,4 +46,26 @@ const pessoa004 = criaPessoa('Thomas', 'Shelby', 24);
 console.log(pessoa001);
 console.log(pessoa002);
 console.log(pessoa003);
-console.log(pessoa004 );
+console.log(pessoa004);
+
+const pessoa0001 = {
+    nome:'Jax',
+    sobrenome: 'Teller',
+    idade: 30,
+
+    fala(){
+        console.log(`A minha idade atual é ${this.idade}.`);//Só funciona nesse contexto
+    },
+
+    incrementaIdade(){
+        ++this.idade;
+    }
+}
+
+pessoa0001.fala(); //Chama o método fala(), dentro do objeto pessoa0001
+pessoa0001.incrementaIdade(); //Chama o método incrementaIdade(), dentro do objeto pessoa0001
+pessoa0001.fala();
+pessoa0001.incrementaIdade();
+pessoa0001.fala();
+pessoa0001.incrementaIdade();
+pessoa0001.fala();
