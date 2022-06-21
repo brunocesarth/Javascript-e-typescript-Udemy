c65473dcda6bfb16b0c561832d43b785b377e137
Bruno Cesar Machado Thobias <bruno.cm.thobias@hotmail.com>
2022-06-20 21:48:16 -0300

    Aula 25 - JS & TS

---
+++ Aula025/index.js
@@ -0,0 +1,20 @@
+const numero = 10;
+
+// SE (numero >= 0 && numero <= 5) ocorrer, faça isso{código}
+//SENÃO, faça isso {código}
+if (numero >= 0 && numero <= 5) {
+    console.log('O número está entre 0 e 5.');
+} else {
+    console.log('O número NÃO está entre 0 e 5.')
+}
+
+//ELSE IF coloca mais condições a serem verificadas
+if (numero >= 0 && numero <= 5) {
+    console.log('O número está entre 0 e 5.');
+} else if (numero >= 6 && numero <= 8) {
+    console.log('O número está entre 6 e 8.');
+} else if (numero >= 9 && numero <= 11) { //Verdadeiro
+    console.log('O número está entre 9 e 11.');
+} else {
+    console.log('O número não está entre 0 e 11.');
+}
