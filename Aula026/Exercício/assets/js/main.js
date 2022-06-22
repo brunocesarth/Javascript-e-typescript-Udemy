ffad1219449d7d5e997d08e935faa4422100c31b
Bruno Cesar Machado Thobias <bruno.cm.thobias@hotmail.com>
2022-06-22 01:11:17 -0300

    Update main.js

--- Aula026/Exercício/assets/js/main.js
+++ Aula026/Exercício/assets/js/main.js
@@ -1,22 +1,21 @@
 function meuEscopo() {
     const form = document.querySelector('.form');
 
-    function calculaIMC(peso, altura) {
-        resultado = peso / (altura * altura);
-        console.log(resultado);
-        return resultado;
-        
-    }
-
     function recebeEventoForm(evento) {
         evento.preventDefault();
 
-        const peso = form.querySelector('.peso');
-        const altura = form.querySelector(Number('.altura'));
-        console.log(altura);
-        calculaIMC(peso, altura);
+        let peso = form.querySelector('.peso');
+        let altura = form.querySelector('.altura');
+        calculaIMC(peso.value, altura.value);
     }
+    
     form.addEventListener('submit', recebeEventoForm);
+
 }
+
+function calculaIMC(pesoFun, alturaFun) {
+    let resultadoFun = pesoFun / (alturaFun * alturaFun);
+    return resultadoFun;
+};
 
 meuEscopo();
