260011d36c4ca180e34e1217be64b8c17e68819b
Bruno Cesar Machado Thobias <bruno.cm.thobias@hotmail.com>
2022-06-22 11:34:19 -0300

    Finalização do exercício - Aula 26 - JS & TS

--- Aula026/Exercício/assets/js/main.js
+++ Aula026/Exercício/assets/js/main.js
@@ -1,21 +1,58 @@
+const mensagem = document.querySelector('.mensagem');
+const container = document.querySelector('.container__mensagem')
+
 function meuEscopo() {
     const form = document.querySelector('.form');
 
     function recebeEventoForm(evento) {
         evento.preventDefault();
-
-        let peso = form.querySelector('.peso');
-        let altura = form.querySelector('.altura');
-        calculaIMC(peso.value, altura.value);
+        calculaIMC();
     }
-    
+
     form.addEventListener('submit', recebeEventoForm);
 
 }
 
-function calculaIMC(pesoFun, alturaFun) {
-    let resultadoFun = pesoFun / (alturaFun * alturaFun);
-    return resultadoFun;
-};
 
 meuEscopo();
+
+function calculaIMC() {
+    let peso = document.querySelector('.peso');
+    let altura = document.querySelector('.altura');
+
+    if (altura.value === '') {
+        container.style.cssText = 'background: rgb(240, 89, 59)';
+        mensagem.innerHTML = `Altura inválida`;
+    } else if (peso.value === '') {
+        container.style.cssText = 'background: rgb(240, 89, 59)';
+        mensagem.innerHTML = `Peso inválido`;
+    } else {
+        peso = Number(peso.value);
+        altura = Number(altura.value);
+        let resultado = peso / (altura * altura);
+        resultado = Number(resultado.toFixed(2));
+        verificaResultado(resultado);
+    }
+}
+
+function verificaResultado(imc) {
+    if (imc < 18.5) {
+        container.style.cssText = 'background-color: rgb(0, 252, 38)';
+        mensagem.innerHTML = `Seu IMC é ${imc}(Abaixo do peso)`;
+    } else if (imc >= 18.5 && imc <= 24.9) {
+        container.style.cssText = 'background-color: rgb(0, 252, 38)';
+        mensagem.innerHTML = `Seu IMC é ${imc}(Peso normal)`;
+    } else if (imc >= 25 && imc <= 29.9) {
+        container.style.cssText = 'background-color: rgb(0, 252, 38)';
+        mensagem.innerHTML = `Seu IMC é ${imc}(Sobrepeso)`;
+    } else if (imc >= 30 && imc <= 34.9) {
+        container.style.cssText = 'background-color: rgb(0, 252, 38)';
+        mensagem.innerHTML = `Seu IMC é ${imc}(Obesidade grau 1)`;
+    } else if (imc >= 35 && imc <= 39.9) {
+        container.style.cssText = 'background-color: rgb(0, 252, 38)';
+        mensagem.innerHTML = `Seu IMC é ${imc}(Obesidade grau 2)`;
+    } else if (imc > 40) {
+        container.style.cssText = 'background-color: rgb(0, 252, 38)';
+        mensagem.innerHTML = `Seu IMC é ${imc}(Obesidade grau 3)`;
+    }
+}
