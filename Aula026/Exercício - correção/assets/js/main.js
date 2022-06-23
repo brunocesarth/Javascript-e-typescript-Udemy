b53647762878209b3c7209f7f45d96ac68122434
Bruno Cesar Machado Thobias <bruno.cm.thobias@hotmail.com>
2022-06-23 18:57:37 -0300

    Finalização da correção do exercício de IMC

--- Aula026/Exercício - correção/assets/js/main.js
+++ Aula026/Exercício - correção/assets/js/main.js
@@ -22,11 +22,12 @@ form.addEventListener('submit', function (e) {
     const nivelImc = getNivelImc(imc);
 
     const msg = `Seu IMC é ${imc} (${nivelImc}).`;
+
     setResultado(msg, true);
 });
 
 function getNivelImc(imc) {
-    const nivel = ['Abaixo do peso', 'Peso normal', 'Sobrepeso', 'Obesidade grau1', 'Obesidade grau 2', 'Obesidade grau 3'];
+    const nivel = ['Abaixo do peso', 'Peso normal', 'Sobrepeso', 'Obesidade grau 1', 'Obesidade grau 2', 'Obesidade grau 3'];
 
     if (imc >= 39.9) return nivel[5];
     if (imc >= 34.9) return nivel[4];
@@ -38,7 +39,7 @@ function getNivelImc(imc) {
 
 function getImc(peso, altura) {
     const imc = peso / altura ** 2;
-    return imc.toFixed / (2);
+    return imc.toFixed(2);
 }
 
 function criaP() {
@@ -50,10 +51,14 @@ function setResultado(msg, isValid) {
     const resultado = document.querySelector('#resultado');
     resultado.innerHTML = '';
 
-    
+
     const p = criaP();
-    if (isValid) p.classList.add('paragrafo-resultado');
-    
+    if (isValid) {
+        p.classList.add('paragrafo-resultado');
+    } else {
+        p.classList.add('bad');
+    }
+
     p.innerHTML = msg;
     resultado.appendChild(p);
 }
