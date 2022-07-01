acc100f8cd5e0b44324a203135aa0b8e31b49971
Bruno Cesar Machado Thobias <bruno.cm.thobias@hotmail.com>
2022-07-01 18:16:25 -0300

    Aula 35 - Correção - JS & TS

---
+++ Aula035/Exercício - Correção/assets/js/main.js
@@ -0,0 +1,24 @@
+const elementos = [
+    {tag: 'p', texto: 'Qualquer texto que você quiser.'},
+    {tag: 'div', texto: 'Frase 2'},
+    {tag: 'section', texto: 'Frase 3'},
+    {tag: 'footer', texto: 'Frase 4'}
+];
+
+const container = document.querySelector('.container');
+const div = document.createElement('div');
+
+for (let i = 0; i < elementos.length; i++) {
+    let {tag, texto} = elementos[i];
+    let tagCriada = document.createElement(tag);
+    tagCriada.innerHTML = texto;
+    div.appendChild(tagCriada);
+} //No caso de somente texto, posso usar o innerText no lugar de innerHTML
+
+container.appendChild(div);
+
+/* 
+    Esse seria outro meio de fazer
+    let textoCriado = document.createTextNode(texto);
+    tagCriada.appendChild(textoCriado)
+*/
