b78c2bfb8f812f305845c594dbd31639ac85741c
Bruno Cesar Machado Thobias <bruno.cm.thobias@hotmail.com>
2022-07-01 01:11:03 -0300

    Update index.js

--- Aula034/index.js
+++ Aula034/index.js
@@ -34,7 +34,7 @@ for (let i = 0; i <= 10; i++) {
     }
 }
 
-const frutas = ['Maçã', 'Pêra', 'Uva','Banana', 'Melão', 'Melancia', 'Jabuticaba'];
+const frutas = ['Maçã', 'Pêra', 'Uva', 'Banana', 'Melão', 'Melancia', 'Jabuticaba'];
 
 for (let i = 0; i < frutas.length; i++) {
     console.log(`Índice ${i}`,frutas[i]);
