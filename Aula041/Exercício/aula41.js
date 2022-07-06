34c7487d18ae044f401d1932eb3b6b545a069440
Bruno Cesar Machado Thobias <bruno.cm.thobias@hotmail.com>
2022-07-06 00:10:10 -0300

    Aula 41 - JS & TS

---
+++ Aula041/Exercício/aula41.js
@@ -0,0 +1,9 @@
+// Escreva uma função que recebe 2 números e retorne o maior deles
+function maior(x, y) {
+    if (x > y) {
+        return x;
+    } else {
+        return y;
+    }
+}
+console.log(maior(4,2));
