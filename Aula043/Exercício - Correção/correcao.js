169319d227a7a8568f13e053cb2700b42dc9c45c
Bruno Cesar Machado Thobias <bruno.cm.thobias@hotmail.com>
2022-07-06 19:14:47 -0300

    Continuação da Aula 43 - JS & TS

--- Aula043/Exercício - Correção/correcao.js
+++ Aula043/Exercício - Correção/correcao.js
@@ -7,20 +7,15 @@
 //Checar se o número é realmente um número = Retorna o próprio número
 //Use a função com números de 0 a 100
 
-function testNumber(num) {
-    if (typeof num !== 'number') return num;
-    if (num % 3 === 0 && num % 5 === 0) {
-        return 'FizzBuzz';
-    } else if (num % 3 === 0) {
-        return 'Fizz'
-    } else if (num % 5 === 0) {
-        return 'Buzz';
-    } else {
-        return num;
-    }
+function fizzBuzz(numero) {
+    if (typeof numero !== 'number') return numero;
+    if (numero % 3 === 0 && numero % 5 === 0) return 'FizzBuzz';
+    if (numero % 3 === 0) return 'Fizz';
+    if (numero % 5 === 0) return 'Buzz';
+    return numero;
 }
-console.log('teste', testNumber('teste'));
 
+console.log('a', fizzBuzz('a'));
 for (let i = 0; i <= 100; i++) {
-    console.log(i, testNumber(i));
+    console.log(i, fizzBuzz(i));
 }
