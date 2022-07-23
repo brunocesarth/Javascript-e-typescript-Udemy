6aad9595daac70071dbaa8fc24ddee7f6a0fd50a
Bruno Cesar Machado Thobias <bruno.cm.thobias@hotmail.com>
2022-07-23 19:14:44 -0300

    Aula 52 - JS & TS

---
+++ Aula052/aula52.js
@@ -0,0 +1,24 @@
+//Escopo léxico
+const nome = 'Luiz';
+
+function falaNome() {
+    console.log(nome);
+} //Aqui ele acessa a variável que foi declarada no escopo global
+falaNome();
+
+function usaFalaNome() {
+    falaNome();
+}
+usaFalaNome();
+
+function falaNome1() {
+    //const nome = 'Ótavio';
+    console.log(nome);
+} //Aqui ele acessa a variável declarada dentro da função 
+falaNome1();
+
+function usaFalaNome1() {
+    const nome = 'Ótavio';
+    falaNome1();
+} //A função lembra onde ela foi declarada, então não adianta criar a variável nome aqui que ela não será executada
+usaFalaNome1();
