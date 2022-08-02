6395efeea36541bdc947ea80e7855cca0832a414
Bruno Cesar Machado Thobias <bruno.cm.thobias@hotmail.com>
2022-08-02 20:42:06 -0300

    Aula 50 - Finalização do exercício - JS & TS

--- Aula059/Exercício/assets/js/main.js
+++ Aula059/Exercício/assets/js/main.js
@@ -1,78 +1,51 @@
-function criaCalculadora() {
-    return {
-        //Atributos ficam em cima
-        display: document.querySelector('.display'),
+//Calculadora com funções construtoras
+function Calculadora() {
+    this.display = document.querySelector('.display');
 
-        //Métodos ficam em baixo
-        inicia() {
-            this.cliqueBotoes();
-            this.pressionaEnter();
-        },
-
-        pressionaEnter() {
-            this.display.addEventListener('keyup', e => {
-                if (e.keyCode === 13) {
-                    this.realizaConta();
-                }
-            })
-        },
-
-        realizaConta() {
-            let conta = this.display.value;
-
-            try {
-                conta = eval(conta);
-                if (!conta) {
-                    alert('Conta inválida');
-                    return;
-                }
-
-                this.display.value = conta;
-            } catch (e) {
-                alert('Conta inválida');
-                return;
-            }
-        },
-
-        clearDisplay() {
-            this.display.value = ' ';
-        },
-
-        apagaUm() {
-            this.display.value = this.display.value.slice(0, -1);
-        },
-
-        cliqueBotoes() {
-            //this aqui é a calculadora
-            document.addEventListener('click', (e) => {
-                const el = e.target;
-
-                if (el.classList.contains('btn-num')) {
-                    this.btnParaDisplay(el.innerText);
-                }
-
-                if (el.classList.contains('btn-clear')) {
-                    this.clearDisplay();
-                }
-
-                if (el.classList.contains('btn-del')) {
-                    this.apagaUm();
-                }
-
-                if (el.classList.contains('btn-eq')) {
-                    this.realizaConta();
-                }
-
-                this.display.focus();
-            });
-        },
-
-        btnParaDisplay(valor) {
-            this.display.value += valor;
-        },
-
+    this.inicia = () => {
+        this.capturaCliques();
+        this.capturaEnter();
     };
+
+    this.capturaEnter = () => {
+        document.addEventListener('keyup', e => {
+            if (e.keyCode !== 13) return;
+            this.realizaConta();
+        });
+    };
+
+    this.capturaCliques = () => {
+        document.addEventListener('click', event => {
+            const el = event.target;
+            if (el.classList.contains('btn-num')) this.addNumDisplay(el);
+            if (el.classList.contains('btn-clear')) this.clear();
+            if (el.classList.contains('btn-del')) this.del();
+            if (el.classList.contains('btn-eq')) this.realizaConta();
+        });
+    };
+
+    this.realizaConta = () => {
+        try {
+            const conta = eval(this.display.value);
+
+            if (!conta) {
+                alert('Conta inválida');
+                return
+            }
+            this.display.value = conta;
+        } catch (e) {
+            alert('Conta inválida');
+            return;
+        }
+    };
+
+    this.addNumDisplay = el => {
+        this.display.value += el.innerText
+        this.display.focus();
+    };
+    this.clear = el => this.display.value = '';
+    this.del = el => this.display.value = this.display.value.slice(0, -1);
 }
 
-const calculadora = criaCalculadora();
-calculadora.inicia();
+const calculadora = new Calculadora(); //Criando o objeto calculadora
+calculadora.inicia(); //Chamando o método inicia
