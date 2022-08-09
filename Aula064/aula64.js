e313d31ea07111f3dc2aa0ee6bfc7b1857d553ce
Bruno Cesar Machado Thobias <bruno.cm.thobias@hotmail.com>
2022-08-09 09:05:49 -0300

    Update aula64.js

--- Aula064/aula64.js
+++ Aula064/aula64.js
@@ -2,5 +2,9 @@
 
 const a1 = [1, 2, 3];
 const a2 = [4, 5, 6];
-const a3 = a1.concat(a2, [7,8,9], 'Luiz');//.concat - usando aqui para concatenar os arrays
+const a3 = a1.concat(a2, [7, 8, 9], 'Luiz');//.concat - usando aqui para concatenar os arrays
 console.log(a3);
+
+//...spread operator
+const a4 = [...a1, 'Luiz', ...a2, ...[7, 8, 9]];
+console.log(a4);
